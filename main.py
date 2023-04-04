f3ff323271d6249980268189fba97317684f9a72
M-Enzinger <max@enzinger.de>
2023-04-04 14:25:12 +0200

    2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
 import streamlit as st
+from deepface import DeepFace
 
 tab1, tab2, tab3, tab4 = st.tabs(["Face Comparison", "Face Recognition", "Face Analyzis", "Crowd Analysis"])
 

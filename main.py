02969dc6231e0c6eb0593e05aa97b73a3505e2c9
M-Enzinger <max@enzinger.de>
2023-04-04 17:07:24 +0200

    2

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ tab1, tab2, tab3, tab4 = st.tabs(["Face Comparison", "Face Recognition", "Single
 with tab3:
     st.header("Image:")
     sfa = st.file_uploader("Choose a picture to analyse.", type=['png', 'jpg', 'img'])
-    st.text(type(sfa))
     if st.button('Analyse'):
-        if (type(sfa) == NonType):
+        if (type(sfa) == NoneType):
             st.error('Upload a File First')
         else:
             a_one = DeepFace.analyze(img_path=sfa,

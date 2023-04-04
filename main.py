b7783c8a4a1c59ea2ffbf53103c57a93c37c0c89
M-Enzinger <max@enzinger.de>
2023-04-04 17:16:01 +0200

    2

--- main.py
+++ main.py
@@ -12,18 +12,19 @@ tab1, tab2, tab3, tab4 = st.tabs(["Face Comparison", "Face Recognition", "Single
 
 with tab3:
     st.header("Image:")
-    sfa = st.file_uploader("Choose a picture to analyse.", type=['png', 'jpg', 'img'])
+    sfa = st.file_uploader("Choose a picture to analyse.", type=['png', 'jpg', 'img', 'jpeg'])
+    sfa_opened = Image.open(sfa)
     if st.button('Analyse'):
         if sfa is None:
             st.error('Upload a File First')
         else:
-            a_one = DeepFace.analyze(img_path=sfa,
+            a_one = DeepFace.analyze(img_path=sfa_opened,
                                      actions=['age', 'gender', 'race', 'emotion']
                                      )
             if len(a_one) == 1:
                 col1, col2, col3 = st.columns(3)
                 with col1:
-                    im = Image.open(sfa)
+                    im = Image.open(sfa_opened)
                     # Create figure and axes
                     fig, ax = plt.subplots()
                     # Display the image

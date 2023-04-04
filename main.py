d0714048f2ad3378f9d4f13938a90cc5ad108347
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2023-04-04 14:08:55 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from deepface import DeepFace
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
+import streamlit as st
+
+tab1, tab2, tab3, tab4 = st.tabs(["Face Comparison", "Face Recognition", "Face Analyzis", "Crowd Analysis"])
+
+with tab1:
+   st.header("Face Comparison")
+   
